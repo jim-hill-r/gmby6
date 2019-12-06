3b3bc2352d009a0ae6931720489cebb481917f8a
Jim Hill <jim.hill.r@gmail.com>
2019-12-05 18:05:44 -0600

    Started decouple of script and config

--- core/infra/provision/iam.py
+++ core/infra/provision/iam.py
@@ -16,6 +16,6 @@ def createUser(username):
         print(f'User {username} probably already exists, not creating.')
 
 if __name__ == '__main__':
-    usernames = ['jim-hill-r','stevenhill']
+    usernames = ['jim-hill-r','stevenhill'] #TODO: Get these from the json resource file
     for username in usernames:
         createUser(username)
