c58ed7c911d4f96e27ae959d2f1c0a3af5feb340
Jim Hill <jim.hill.r@gmail.com>
2019-12-06 16:47:27 -0600

    Appsync provision script idempotent

--- api/infra/provision/appsync.py
+++ api/infra/provision/appsync.py
@@ -4,11 +4,17 @@ except ImportError:
    print(f'Error: boto3 is required. Please install.')
    print(f'Try: pip install boto3')
 
-def createGraphQL():
-    appsync = boto3.client('appsync')
-    print(f'Creating graphQL in appsync...')
-    appsync.create_graphql_api(name='gmby6-gql',authenticationType='API_KEY')
-    print(f'GraphQL created.')
+def createGraphQL(region):
+   name = 'gmby6-gql'
+   appsync = boto3.client('appsync',region_name=region)
+   print(f'Creating graphQL in appsync...')
+   response = appsync.list_graphql_apis()
+   for api in response['graphqlApis']:
+      if(api['name'] == name):
+         print(f'GraphQL {name} already exists.')
+         return
+   appsync.create_graphql_api(name=name,authenticationType='API_KEY')
+   print(f'GraphQL created.')
 
 if __name__ == '__main__':
-    createGraphQL()
+    createGraphQL('us-east-2')
