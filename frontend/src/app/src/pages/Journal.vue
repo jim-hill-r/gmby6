cf724bfc73f19c46f8b2e923e6cca9071eccd57b
Jim Hill <jim.hill.r@gmail.com>
2019-12-07 08:14:03 -0600

    Add + Post button in Journal

--- frontend/src/app/src/pages/Journal.vue
+++ frontend/src/app/src/pages/Journal.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    Journal
-  </div>
+  <q-page padding>
+    <q-card class="my-card" v-for="post in posts" v-bind:key="post.id">
+      <q-card-section>
+        {{ post.text }}
+      </q-card-section>
+    </q-card>
+
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="add" color="accent" />
+    </q-page-sticky>
+  </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageJournal'
+  name: 'PageJournal',
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          text: 'Hello, world!'
+        },
+        {
+          id: 2,
+          text: 'Second Post'
+        }
+      ]
+    }
+  }
 }
 </script>
