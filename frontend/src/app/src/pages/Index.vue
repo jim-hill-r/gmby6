369e7914b72303c91f256d6b23aeafba9c61e635
Jim Hill <jim.hill.r@gmail.com>
2019-12-06 23:05:18 -0600

    Updated all icons to be gumby

--- frontend/src/app/src/pages/Index.vue
+++ frontend/src/app/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/gumby-192x192.png">
+    <img alt="Quasar logo" src="~assets/gumby-logo-full.svg" style="width: 40vw; height: 40vh;">
   </q-page>
 </template>
 
