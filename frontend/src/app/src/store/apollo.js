e36d2b3a7b9fc5a175819ada8099781f0e11637c
Jim Hill <jim.hill.r@gmail.com>
2019-12-07 18:51:56 -0600

    auth via api key added

--- frontend/src/app/src/store/apollo.js
+++ frontend/src/app/src/store/apollo.js
@@ -5,7 +5,11 @@ import { createHttpLink } from 'apollo-link-http'
 
 const httpLink = createHttpLink({
   uri: 'https://v4oubrsjlrhhpmyvrgx34yhheu.appsync-api.us-east-2.amazonaws.com/graphql',
-  fetch: fetch
+  fetch: fetch,
+  headers: {
+    'x-api-key': 'da2-oryzmxetwnhrdlkgwkfda3npai',
+    'content-type': 'application/graphql'
+  }
 })
 
 export default new ApolloClient({
