c619031b39ddb40b33da391c2c7faad22dfea645
Jim Hill <jim.hill.r@gmail.com>
2019-12-28 15:18:54 -0600

    Updated api key

--- frontend/src/app/src/store/apollo.js
+++ frontend/src/app/src/store/apollo.js
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({
   uri: 'https://v4oubrsjlrhhpmyvrgx34yhheu.appsync-api.us-east-2.amazonaws.com/graphql',
   fetch: fetch,
   headers: {
-    'x-api-key': 'da2-oryzmxetwnhrdlkgwkfda3npai',
+    'x-api-key': 'da2-erl577lv4fdblpbzlx3yen6ydq',
     'content-type': 'application/graphql'
   }
 })
